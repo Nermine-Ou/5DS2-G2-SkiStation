ae5ab9f191d268b1380f9f9e514220d0afc38c96
chaker <chaker.rhim@esprit.tn>
2024-11-02 17:38:56 +0100

    change everthing

--- src/main/java/tn/esprit/spring/services/CourseServicesImpl.java
+++ src/main/java/tn/esprit/spring/services/CourseServicesImpl.java
@@ -1,56 +1,37 @@
 package tn.esprit.spring.services;
 
 import lombok.AllArgsConstructor;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 import tn.esprit.spring.entities.Course;
+import tn.esprit.spring.entities.TypeCourse;
 import tn.esprit.spring.repositories.ICourseRepository;
 
 import java.util.List;
-
 @AllArgsConstructor
 @Service
-public class CourseServicesImpl implements ICourseServices {
-
-    // Initialize the logger
-    private static final Logger logger = LogManager.getLogger(CourseServicesImpl.class);
+public class CourseServicesImpl implements  ICourseServices{
 
     private ICourseRepository courseRepository;
 
     @Override
     public List<Course> retrieveAllCourses() {
-        logger.info("Retrieving all courses");
-        List<Course> courses = courseRepository.findAll();
-        logger.debug("Number of courses retrieved: {}", courses.size());
-        return courses;
+        return courseRepository.findAll();
     }
 
     @Override
     public Course addCourse(Course course) {
-        logger.info("Adding a new course: {}", course);
-        Course savedCourse = courseRepository.save(course);
-        logger.debug("Course added with ID: {}", savedCourse.getNumCourse());
-        return savedCourse;
+        return courseRepository.save(course);
     }
 
     @Override
     public Course updateCourse(Course course) {
-        logger.info("Updating course with ID: {}", course.getNumCourse());
-        Course updatedCourse = courseRepository.save(course);
-        logger.debug("Course updated: {}", updatedCourse);
-        return updatedCourse;
+        return courseRepository.save(course);
     }
 
     @Override
     public Course retrieveCourse(Long numCourse) {
-        logger.info("Retrieving course with ID: {}", numCourse);
-        Course course = courseRepository.findById(numCourse).orElse(null);
-        if (course != null) {
-            logger.debug("Course found: {}", course);
-        } else {
-            logger.warn("No course found with ID: {}", numCourse);
-        }
-        return course;
+        return courseRepository.findById(numCourse).orElse(null);
     }
+
+
 }
